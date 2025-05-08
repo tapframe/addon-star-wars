745749f2f9391fb6384393be75996e57c2eaad9b
tapframe <nayifveliya99@gmail.com>
2025-05-08 14:36:50 +0530

    Add cache control headers to the configuration page for improved resource management

--- src/server.js
+++ src/server.js
@@ -273,6 +273,10 @@ function getAllCatalogs() {
 
 // Define the configuration page
 app.get('/configure', (req, res) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
     res.sendFile(path.join(__dirname, 'public', 'configure.html'));
 });
 
